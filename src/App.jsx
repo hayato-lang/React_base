0ecf13fbf3793f478fb4e2321e914fc31f2ca663
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-03 08:03:03 +0900

    useStateを知る

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onclickCount = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onclickCount}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwichShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ^ω^ )</p>}
     </>
   );
 };
